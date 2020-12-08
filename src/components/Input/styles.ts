9a78f658a1201e681bae5667bc02962ba03cbb1b
Juangomes07 <72666763+Juangomes07@users.noreply.github.com>
2020-12-08 16:13:21 -0300

    ATT

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 export const Container = styled.div<ContainerProps>`
   background: #232129;
@@ -20,6 +23,12 @@ export const Container = styled.div<ContainerProps>`
     margin-top: 8px;
   }
   ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
     props.isFocused &&
     css`
       color: #ff9000;
@@ -30,6 +39,7 @@ export const Container = styled.div<ContainerProps>`
     css`
       color: #ff9000;
     `}
+
   input {
     flex: 1;
     background: transparent;
@@ -43,5 +53,22 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,39 +1,86 @@
-import React from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import { AuthContext } from '../../context/AuthContext';
+import getValidationErros from '../../utils/getValidationErros';
+
 import logoImg from '../../assets/logo.svg';
 
-import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
-      <form>
-        <h1>Faça seu Logon</h1>
+import { Container, Content, Background } from './styles';
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-        <Button type="submit">Entrar</Button>
+  const { signIn } = useContext(AuthContext);
 
-        <a href="forgot">Esqueci minha senha </a>
-      </form>
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .email('Digite um e-mail válido')
+            .required('E-mail obrigatório'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
 
-      <a href="Sign">
-        <FiLogIn />
-        Criar conta{' '}
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        const errors = getValidationErros(err);
+
+        formRef.current?.setErrors(errors);
+      }
+    },
+    [signIn],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu Logon</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha </a>
+        </Form>
+
+        <a href="Sign">
+          <FiLogIn />
+          Criar conta{' '}
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
